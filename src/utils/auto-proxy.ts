22ee4f1bcb71c25ad777a7c89ea7ff147bcbc0a2
= <=>
2024-07-22 15:44:31 -0700

    update: auto-proxy function

--- src/utils/auto-proxy.ts
+++ src/utils/auto-proxy.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-export async function getProxies() {
-  const data = await axios.get(
-    "https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&protocol=http&proxy_format=protocolipport&format=json&timeout=619",
-    {
-        timeout: 6000
-    }
-  );
-  return data.data.proxies
+export async function getProxies(
+  url = "https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&protocol=http&proxy_format=protocolipport&format=json&timeout=619",
+  method = "GET"
+) {
+  const data = await axios.get(url, {
+    timeout: 6000,
+    method: method,
+  });
+  return data.data.proxies;
 }
